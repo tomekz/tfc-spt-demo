b37b063a76dcbe3ee14560192a19978cf4129054
Tomasz Zadrożny <zadrozny.tomasz@gmail.com>
2025-09-11 14:29:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,5 +3,10 @@ resource "null_resource" "example" {
     command = "echo Hello, World! ${var.message}"
   }
 }
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo Hello, World! ${var.message}"
+  }
+}
 
 
